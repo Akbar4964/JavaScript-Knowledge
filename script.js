f9eac7a225a765ee44f770dca51ca85de27be600
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-08-25 11:23:22 +0500

    Added new variables to script file

---
+++ script.js
@@ -0,0 +1,49 @@
+// ALERT
+// alert("Hello world!")
+
+// alert("My name is Akbar")
+
+// alert("How are you?")
+
+// VARIABLE --> VARIABLE NAME --> = --> VARIABLE VALUE
+// let name = "Akbar";
+// console.log(name);
+
+// let surname = "Shuhratullayev";
+// console.log(surname);
+
+// let age = 17;
+// console.log(age);
+
+// let ism = "Akbar";
+// console.log(ism);
+
+// let familiya = "Shuhratullayev";
+// console.log(familiya);
+
+// let yosh = 17;
+// console.log(yosh);
+
+// let userName = "Akbar";
+// console.log(userName);
+
+// let userSurname = "Shuhratullayev";
+// console.log(userSurname);
+
+// let userAge = 17;
+// console.log(userAge);
+
+// VARIABLEDA ISHLATISH MUMKIN BO'LMAGAN HOLATLAR
+// let with name = "Akbar";
+// console.log(name);
+
+// let 1surname = "Shuhratullayev";
+// console.log(1surname);
+
+// let @ge = 17;
+// console.log(@ge);
+
+// let for = "SomeThing";
+// console.log(for);
+// let var = "SomeThing";
+// console.log(var);
