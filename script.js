ea748fb6f0ba3d05bf07185c372f7fb9eb2ea960
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-08-25 16:15:32 +0500

    added new codes

--- script.js
+++ script.js
@@ -190,3 +190,28 @@
 // console.log(c);
 // b %= c;
 // console.log(b);
+
+// LOGICAL OPERATORS
+// let a = 5;
+// let b = 6;
+// console.log(a && b);
+// console.log(a || b);
+// console.log(!a);
+
+// TRUTHY AND FALSY
+// FALSY
+// console.log(Boolean(false));
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+// console.log(Boolean(0n));
+
+// TRUTHY
+// console.log(Boolean("Akbar"));
+// console.log(Boolean(1));
+// console.log(Boolean([]));
+// console.log(Boolean({}));
+// console.log(Boolean("0"));
+// console.log(Boolean("false"));
