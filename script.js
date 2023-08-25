f02b3800e78f786d7bf7860aa6443e2e39a40d53
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-08-25 11:49:24 +0500

    Added Primitive types

--- script.js
+++ script.js
@@ -47,3 +47,77 @@
 // console.log(for);
 // let var = "SomeThing";
 // console.log(var);
+
+// MA'LUMOT TURLARI --> PRIMITIVE AND OBJECT
+// PRIMITIVE TYPES
+
+// NUMBER TYPE
+// let age = 17;
+// console.log(age);
+
+// let price = 29.0;
+// console.log(price);
+
+// let cost = 99;
+// console.log(cost);
+
+// STRING TYPE
+// let name = "Akbar";
+// console.log(name);
+
+// let surname = "Shuhratullyev";
+// console.log(surname);
+
+// let fatherName = "Bahodir";
+// console.log(fatherName);
+
+// BOOLEAN TYPE
+// let user = true;
+// console.log(user);
+
+// let users = false;
+// console.log(users);
+
+// UNDEFINED TYPE
+// let user;
+// console.log(user);
+
+// let users;
+// console.log(users);
+
+// NULL TYPE
+// let name = null;
+// console.log(name);
+
+// let surname = null;
+// console.log(surname);
+
+// let age = null;
+// console.log(age);
+
+// SYMBOL TYPE
+// let id = Symbol("Akbar");
+// console.log(id);
+
+// let id1 = Symbol("Shuhratullayev");
+// console.log(id1);
+
+// let id2 = Symbol(17);
+// console.log(id2);
+
+// BIGINT TYPE
+// let num1 = BigInt(999954964);
+// console.log(num1);
+
+// let num2 = BigInt(1234567890);
+// console.log(num2);
+
+// let num3 = BigInt(987654321);
+// console.log(num3);
+
+// OPERATOR TYPEOF
+// let txt = "Akbar";
+// console.log(typeof txt);
+
+// let num = 17;
+// console.log(typeof num);
