f6ef38b3fcafadcc1f34759ef1201f8910cba024
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-09 14:46:25 +0500

    added codes

--- script.js
+++ script.js
@@ -271,44 +271,44 @@
 // console.log(Boolean("false"));
 
 // const x = Math.trunc(Math.random() * 8);
-// // const x = 1;
-// switch (x) {
-//   case 1:
-//     {
-//       console.log("Monday");
-//     }
-//     break;
-//   case 2:
-//     {
-//       console.log("Tuesday");
-//     }
-//     break;
-//   case 3:
-//     {
-//       console.log("Wednesday");
-//     }
-//     break;
-//   case 4:
-//     {
-//       console.log("Thursday");
-//     }
-//     break;
-//   case 5:
-//     {
-//       console.log("Friday");
-//     }
-//     break;
-//   case 6:
-//     {
-//       console.log("Saturday");
-//     }
-//     break;
-//   case 7:
-//     {
-//       console.log("Sunday");
-//     }
-//     break;
-//   default: {
-//     console.log("NO 😂 DATE");
-//   }
-// }
+const x = 1;
+switch (x) {
+  case 1:
+    {
+      console.log("Monday");
+    }
+    break;
+  case 2:
+    {
+      console.log("Tuesday");
+    }
+    break;
+  case 3:
+    {
+      console.log("Wednesday");
+    }
+    break;
+  case 4:
+    {
+      console.log("Thursday");
+    }
+    break;
+  case 5:
+    {
+      console.log("Friday");
+    }
+    break;
+  case 6:
+    {
+      console.log("Saturday");
+    }
+    break;
+  case 7:
+    {
+      console.log("Sunday");
+    }
+    break;
+  default: {
+    console.log("NO 😂 DATE");
+  }
+}
